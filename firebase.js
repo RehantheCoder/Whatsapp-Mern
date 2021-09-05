be7877ce17eb6b99569bffb6619a9ab278350342
Shaik Rehan <abacusrehan@gmail.com>
2021-09-05 19:01:51 +0530

    First Commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqonF7975HgjXyueINmPwSH30U_GOTXmg",
+    authDomain: "whatsapp-mern-6e1d1.firebaseapp.com",
+    projectId: "whatsapp-mern-6e1d1",
+    storageBucket: "whatsapp-mern-6e1d1.appspot.com",
+    messagingSenderId: "414600915791",
+    appId: "1:414600915791:web:10390970b97f21c951b33a"
+};
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Css/Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div>
+            <h1>This is Sidebar</h1>
+        </div>
+    )
+}
+
+export default Sidebar
